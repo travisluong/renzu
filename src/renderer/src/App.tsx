f5f588302b4a31c2a1c5f263a78c3cc58ebdaf84
Travis Luong <travis@travisluong.com>
2025-11-22 21:02:28 +0800

    set up react router

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,34 +1,23 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Routes, Route, Link } from 'react-router-dom'
+import OriginalApp from './archive/App.original'
+
+function Home(): React.JSX.Element {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <nav>
+        <Link to="/original">View Original App</Link>
+      </nav>
+    </div>
+  )
+}
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/original" element={<OriginalApp />} />
+    </Routes>
   )
 }
 
