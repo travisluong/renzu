92af9f270d707415410a2a05bb8cf473722640cb
Travis Luong <travis@travisluong.com>
2025-11-23 17:07:09 +0800

    style home page

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom'
+import styles from '../styles/Home.module.css'
 
 function Home(): React.JSX.Element {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <nav>
-        <Link to="/clusters">View ECS Clusters</Link>
-        <br />
-        <Link to="/original">View Original App</Link>
-      </nav>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Renzu</h1>
+        <p className={styles.subtitle}>AWS ECS Container Management</p>
+      </div>
+
+      <div className={styles.buttonGroup}>
+        <Link to="/clusters" className={styles.primaryButton}>
+          View ECS Clusters
+        </Link>
+        {/* <Link to="/original" className={styles.secondaryButton}>
+          View Original App
+        </Link> */}
+      </div>
     </div>
   )
 }
